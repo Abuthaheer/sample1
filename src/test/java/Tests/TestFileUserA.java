ef1a8bef617429ebfbb9c244a498046f5ebaabe1
Abuthaheer Sameemullha <35775@Hexaware.com>
2020-01-09 09:29:45 +0530

    Added UserA

---
+++ src/test/java/Tests/TestFileUserA.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "Requirements/HomePage.feature", glue = "stepDefinition", dryRun = false, plugin = {
+		"pretty", "junit:XMLResults/report/cucumber.xml",
+		"rerun:target/rerun.txt" }, monochrome = true, tags = { "~@ignore" })
+public class TestRunner {
+
+}
